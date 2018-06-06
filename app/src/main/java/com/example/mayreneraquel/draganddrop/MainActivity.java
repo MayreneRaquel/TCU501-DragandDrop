d983090e29372765b84ee9f8e61adbb5c723fb53
Mayrene Anchia Quesada <b40371@ecci.ucr.ac.cr>
2018-06-06 17:03:39 -0600

    Pantalla de inicio

---
+++ app/src/main/java/com/example/mayreneraquel/draganddrop/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.mayreneraquel.draganddrop;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void pantallitaReci(View v){
+        //Intent i = new Intent(this, ReciclajeActivity.class);
+        //startActivity(i);
+    }
+
+}
