3bd71b67a1ea8b3a34fd84a4efd9991cacadbbd8
Patrick <patrickrx7@gmail.com>
2018-06-09 17:17:12 -0600

    Hacer funcionalidad drag and drop y agregar imagenes random

--- app/src/main/java/com/example/mayreneraquel/draganddrop/GameActivity.java
+++ app/src/main/java/com/example/mayreneraquel/draganddrop/GameActivity.java
@@ -1,13 +1,133 @@
 package com.example.mayreneraquel.draganddrop;
 
+import android.content.ClipData;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
+    ImageView img1, img2, img3;
+    TextView text1;
+
+    public int[] imglist = {
+        R.drawable.airpollute,
+        R.drawable.chemicalswaste,
+        R.drawable.deforestation,
+        R.drawable.electronictrash,
+        R.drawable.forestfire,
+        R.drawable.huntinganimals,
+        R.drawable.plasticpollute,
+        R.drawable.trashriver,
+        R.drawable.wastingwater,
+        R.drawable.waterpollute
+    };
+
+    public int[] noimglist = {
+        R.drawable.huntinganimals,
+        R.drawable.plasticpollute,
+        R.drawable.trashriver,
+        R.drawable.wastingwater,
+        R.drawable.waterpollute,
+        R.drawable.electronictrash,
+        R.drawable.forestfire,
+        R.drawable.airpollute,
+        R.drawable.chemicalswaste,
+        R.drawable.deforestation,
+    };
+
+    public String[] fraseslist = {
+            "Air pollution",
+            "Chemical waste",
+            "Deforestation",
+            "Electronic trash",
+            "Forest Fire",
+            "Hunting animals",
+            "Plastic pollution",
+            "Trash rivers",
+            "Wasting water",
+            "Water pollution"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        img1 = (ImageView) findViewById(R.id.img1);
+        img2 = (ImageView) findViewById(R.id.img2);
+        img3 = (ImageView) findViewById(R.id.img3);
+        text1 = (TextView) findViewById(R.id.text);
+
+        img1.setOnLongClickListener(longClickListener);
+        img2.setOnLongClickListener(longClickListener);
+        img3.setOnDragListener(dragListener);
+
+        preparar();
     }
+
+    public void preparar() {
+        //Imagenes al azar
+        Random random = new Random();
+        int aux = random.nextInt(imglist.length);
+        int aux2 = random.nextInt(1);
+        int imgSelec1 = imglist[aux];
+        int imgSelec2 = noimglist[aux];
+        if (aux2 == 0) {
+            img1.setImageResource(imgSelec1);
+            img2.setImageResource(imgSelec2);
+        }
+        else {
+            img1.setImageResource(imgSelec2);
+            img2.setImageResource(imgSelec1);
+        }
+        //frase
+        String frase = "\n" + fraseslist[aux];
+        text1.setText(frase);
+    }
+
+    View.OnLongClickListener longClickListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            ClipData d = ClipData.newPlainText("","");
+            View.DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+            v.startDrag(d, shadowBuilder,v, 0);
+            return true;
+        }
+    };
+
+    View.OnDragListener dragListener = new View.OnDragListener() {
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            int dragEvent = event.getAction();
+            final View view = (View) event.getLocalState();
+            Drawable drawi = null;
+            switch (dragEvent) {
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    //img2.setImageResource(R.color.colorPrimaryDark);
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    break;
+                case DragEvent.ACTION_DROP:
+                    if (view.getId() == R.id.img1) {
+                        drawi = img1.getDrawable();
+                        //img1.setImageResource(android.R.color.transparent);
+                        img3.setImageDrawable(drawi);
+                    }
+                    if (view.getId() == R.id.img2) {
+                        drawi = img2.getDrawable();
+                        //img2.setImageResource(android.R.color.transparent);
+                        img3.setImageDrawable(drawi);
+                    }
+                    break;
+            }
+            return true;
+        }
+    };
 }
--- app/src/main/java/com/example/mayreneraquel/draganddrop/MainActivity.java
+++ app/src/main/java/com/example/mayreneraquel/draganddrop/MainActivity.java
@@ -7,24 +7,38 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnPlay;
+    private Button btn1;
+    private Button btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnPlay = (Button) findViewById(R.id.btn1);
-        btnPlay.setOnClickListener(new View.OnClickListener() {
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openGameActivity();
+                openGameActivity1();
+            }
+        });
+
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGameActivity2();
             }
         });
     }
 
-    public void openGameActivity() {
+    public void openGameActivity1() {
         Intent i = new Intent(this, GameActivity.class);
+        startActivity(i);
+    }
+
+    public void openGameActivity2() {
+        Intent i = new Intent(this, GameActivity2.class);
         startActivity(i);
     }
 
